3f5b6367729df39fa57a318a64ccb1582592f0d8
VivanVatsa <VivanVatsa@users.noreply.github.com>
2020-08-22 14:03:38 +0530

    CLI Loading JSON in PY

---
+++ CLI-loadingJSON.py
@@ -0,0 +1,15 @@
+data["hello"]
+['Expression of greeting used by two or more people who meet each other.']
+>> > cls
+Traceback(most recent call last):
+    File "<stdin>", line 1, in <module >
+NameError: name 'cls' is not defined
+>> > clear
+Traceback(most recent call last):
+    File "<stdin>", line 1, in <module >
+NameError: name 'clear' is not defined
+>> > cls
+Traceback(most recent call last):
+    File "<stdin>", line 1, in <module >
+NameError: name 'cls' is not defined
+>> >
