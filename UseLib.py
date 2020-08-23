931cb4ca0a0ec22b094c3211b8f78481e60f9716
VivanVatsa <VivanVatsa@users.noreply.github.com>
2020-08-23 18:38:42 +0530

    Lib used so far,,,,

--- UseLib.py
+++ UseLib.py
@@ -1,6 +1,11 @@
 import difflib
 from difflib import SequenceMatcher
+from difflib import get_close_matches
+
 print(SequenceMatcher(None, "rainn", "rain"))
 
 print(SequenceMatcher(None, "rainn", "rain").ratio())
 print(SequenceMatcher(None, "rain", "rain").ratio())
+print(get_close_matches("raiin", ["help", "pyramid", "rain"]))
+
+# print(data.keys())
